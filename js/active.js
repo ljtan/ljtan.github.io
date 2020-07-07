f1366d4abae118060c72bea35e4df6d9eb05365a
ljtan <ltan0238@bths.edu>
2020-07-07 06:38:17 -0400

    footer

--- js/active.js
+++ js/active.js
@@ -180,6 +180,15 @@
         $.preventDefault()
     });
 
+     window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 
 var newDate = new Date();
 newDate.setDate(newDate.getDate() + 1);
