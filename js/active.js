04bc903bfc0836b3e5d7d04ae078428c125cdd72
ljtan <ltan0238@bths.edu>
2020-07-07 20:32:15 -0400

    owl

--- js/active.js
+++ js/active.js
@@ -99,13 +99,10 @@
             nav: true,
             navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
             responsive: {
-                320: {
+                0: {
                     items: 1
                 },
-                576: {
-                    items: 2
-                },
-                992: {
+                767: {
                     items: 3
                 }
             }
